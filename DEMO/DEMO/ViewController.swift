928be87c27eb2dce6772b642b344ff54b95aac8b
许亚光 <xu_yaguang@163.com>
2022-05-31 00:49:08 +0800

    no message

--- DEMO/DEMO/ViewController.swift
+++ DEMO/DEMO/ViewController.swift
@@ -12,14 +12,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let clayer = MyLayer()
+        clayer.bounds = view.frame
+        clayer.position = view.center
+        clayer.gradientCenter = view.center
+        view.layer.addSublayer(clayer)
+        
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        KxProgressHUD.set(defaultStyle: .dark)
-        KxProgressHUD.showSuccesswithStatus("加载成功")
-        KxProgressHUD.dismissWithDelay(2)
+//        KxProgressHUD.set(defaultStyle: .dark)
+//        KxProgressHUD.set(defaultMaskType: .gradient)
+//        KxProgressHUD.showSuccesswithStatus("加载成功")
+//        KxProgressHUD.dismissWithDelay(4)
+        view.layer.sublayers?.first?.setNeedsDisplay()
     }
 
 }
 
+
+class MyLayer: CALayer {
+    
+    var gradientCenter = CGPoint.zero
+    
+    override func draw(in ctx: CGContext) {
+        super.draw(in: ctx)
+        
+        let locationsCount: Int = 2
+        let locations: [CGFloat] = [0.0, 1.0]
+        let colors: [CGFloat] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.75]
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        if let gradient = CGGradient(colorSpace: colorSpace, colorComponents: colors, locations: locations, count: locationsCount) {
+            let radius = min(bounds.size.width, bounds.size.height)
+            ctx.drawRadialGradient(gradient, startCenter: gradientCenter, startRadius: 0, endCenter: gradientCenter, endRadius: radius, options: CGGradientDrawingOptions.drawsAfterEndLocation)
+        }
+        
+    }
+    
+}
